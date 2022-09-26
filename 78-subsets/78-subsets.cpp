7bce2119b4a8b758e299ccd567d6e6511b611831
AviX4 <56990359+AviX4@users.noreply.github.com>
2022-09-26 21:27:47 +0530

    Time: 19 ms (5.60%), Space: 17.5 MB (8.74%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,21 +3,20 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> op;
-        solve(op,nums,res);
+        solve(op,nums,res,0);
         return res;
     }
 private:
-    void solve(vector<int> op,vector<int> nums,vector<vector<int>>& res){
-        if(nums.size()==0){
+    void solve(vector<int> op,vector<int> nums,vector<vector<int>>& res,int i){
+        if(i==nums.size()){
             res.push_back(op);
             return;
         }
         vector<int> op1=op;
         vector<int>op2=op;
-        op2.push_back(nums[0]);
-        nums.erase(nums.begin());
-        solve(op1,nums,res);
-        solve(op2,nums,res);
+        op2.push_back(nums[i]);
+        solve(op1,nums,res,i+1);
+        solve(op2,nums,res,i+1);
         return;
     }
 };
