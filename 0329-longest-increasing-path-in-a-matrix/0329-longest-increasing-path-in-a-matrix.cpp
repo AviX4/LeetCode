c93ac125829facf1fab13039cb3178d35251c379
AviX4 <56990359+AviX4@users.noreply.github.com>
2022-12-02 08:16:39 +0530

    Time: 89 ms (66.88%), Space: 14.9 MB (94.32%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    map <pair<int,int>,int> dp;
+    int t[201][201];
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int res=0;
+        memset(t,-1,sizeof(t));
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 res=max(solve(matrix,i,j,-1),res);
@@ -15,8 +16,8 @@ public:
         if(i>=matrix.size()||i<0||j<0||j>=matrix[0].size()||matrix[i][j]<=pre ){
             return 0;
         }
-        if(dp.find({i,j})!=dp.end()){
-            return dp[{i,j}];
+        if(t[i][j]!=-1){
+            return t[i][j];
         }
         int result=1;
         result = max(result,1+solve(matrix,i-1,j,matrix[i][j]));
@@ -24,6 +25,6 @@ public:
         result = max(result,1+solve(matrix,i+1,j,matrix[i][j]));
         result = max(result,1+solve(matrix,i,j+1,matrix[i][j]));
         
-        return dp[{i,j}]=result;
+        return t[i][j]=result;
     }
 };
